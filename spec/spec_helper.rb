f23524ffec6c060598221061e0771de34f5e3446
Tyler Pickett <t.pickett66@gmail.com>
2013-05-07 12:44:44 -0500

    fix vcr
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,6 @@ Turn.config do |config|
 end
 
 VCR.configure do |config|
-  config.cassette_library = 'spec/vcr_cassettes'
+  config.cassette_library_dir = 'spec/vcr_cassettes'
   config.hook_into :webmock
 end
