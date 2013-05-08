7bdd0c25bd0da1761dcc8bc8fc4f95f10dda3927
Tyler Pickett <t.pickett66@gmail.com>
2013-05-08 17:02:22 -0500

    Actually fill in that step w/ what I think we need to do
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- features/step_definitions/envelope_steps.rb
+++ features/step_definitions/envelope_steps.rb
@@ -1,6 +1,24 @@
 Given(/^an envelope with a document constucted in ruby$/) do
-  @envelope = Philographer::Envelope.new
-  #the rest of an envelope setup.
+  @envelope = Philographer::Envelope.new({
+    emailBlurb: 'Test email is testing',
+    emailSubject: 'Test email',
+  })
+  path = File.expand_path('../../test_files/basic.pdf', __FILE__)
+  @envelope.documents << Philographer::Document.new(path.to_s)
+
+  recipient = Philographer::Recipient.new({
+    type: 'signer',
+    email: 'test@test.com',
+    name: 'Jon Doe'
+  })
+  recipient.tabs << Philographer::Tab.new({
+    type: 'signHere',
+    x_position: 100,
+    y_position: 100,
+    document_id: 0,
+    page_number: 1
+  })
+  @envelope.recipients << recipient
 end
 
 When(/^the constucted envelope has been submitted to DocuSign$/) do
