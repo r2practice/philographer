d6b8038ac83c8d5b3e57a844fe887bb0be23e05b
Tyler Pickett <t.pickett66@gmail.com>
2013-05-08 17:10:04 -0500

    add an Envelope model to kick things off
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

---
+++ lib/philographer/envelope.rb
@@ -0,0 +1,39 @@
+module Philographer
+  class Envelope
+    include APIObject
+    attribute :status, required: true
+    attribute :accessibility
+    attribute :allow_markup
+    attribute :allow_reassign
+    attribute :allow_recipient_recursion
+    attribute :asynchronous
+    attribute :authoritative_copy
+    attribute :auto_navigation
+    attribute :brand_id
+    attribute :composite_templates
+    attribute :custom_fields
+    attribute :documents#, required: ->(envelope) { envelope.template_id.blank? }, type: Document
+    attribute :email_blurb
+    attribute :email_subject, required: true
+    attribute :enable_wet_sign
+    attribute :enforce_signer_visibility
+    attribute :envelope_id_stamping
+    attribute :event_notification#, type: EventNotification
+    attribute :message_locked
+    attribute :recipients#, type: Recipient
+    attribute :signing_location
+    attribute :template_id
+    attribute :template_roles#, required: ->(envelope) { envelope.template_id.present? }, type: Recipient
+
+    def initialize(attributes = {})
+      super
+      self.status ||= 'created'
+      self.documents ||= []
+      self.recipients ||= []
+    end
+
+    def save
+      Client.post(self)
+    end
+  end
+end
---
+++ spec/philographer/envelope_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+module Philographer
+  describe Envelope do
+    let(:envelope) { Envelope.new }
+
+    describe '#initialize' do
+      it 'must set the status to the default of "created"' do
+        envelope.status.must_equal 'created'
+      end
+
+      it 'must set documents to the default of []' do
+        envelope.documents.must_equal []
+      end
+
+      it 'must set the recipients to the default of []' do
+        envelope.recipients.must_equal []
+      end
+
+      describe 'with an attributes hash supplied' do
+        let(:envelope) {
+          Envelope.new({
+            status: 'sent',
+            signing_location: 'InPerson'
+          })
+        }
+
+        it 'must set the attributes as specified in the hash' do
+          envelope.status.must_equal 'sent'
+          envelope.signing_location.must_equal 'InPerson'
+        end
+      end
+    end
+
+    describe '#save' do
+      it "isn't implemented yet" do
+        skip "We're working on getting a full envelope made"
+      end
+    end
+  end
+end
